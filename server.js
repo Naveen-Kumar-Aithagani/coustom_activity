7a9bf2787911def33f7bf366500aacf453c94279
Naveen Kumar <165649365+Naveen-Kumar-Aithagani@users.noreply.github.com>
2025-07-26 02:47:41 +0530

    icon

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json());
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve the icon file specifically
+app.get('/icon.png', (req, res) => {
+  res.sendFile(path.join(__dirname, 'icon.png'));
+});
+
 // Endpoint for Journey Builder to load the configuration UI
 app.get('/public/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
